d7bd879d448a039418b88394faa2a8eeb2d308a6
Morillo9 <wintermute1992@gmail.com>
2016-07-24 01:39:14 +0200

    update

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import json, urllib
 import os
 
+cur_comic = 'http://xkcd.com/info.0.json'
+cur_url = urllib.urlopen(cur_comic)
+cur_data = json.loads(cur_url.read())
+cur_comic_ID = cur_data['num']
 
-for i in range(1, 1710):
+if not os.path.exists('comics'):
+    os.makedirs('comics')
+
+for i in range(1, cur_comic_ID):
     try:
         url = "http://xkcd.com/" + str(i) +"/info.0.json"
         response = urllib.urlopen(url)
